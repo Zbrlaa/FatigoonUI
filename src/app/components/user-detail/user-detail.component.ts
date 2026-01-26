7d7b6d3494a81532d1daf9c23cebc542fb3de701
Zbrlaa <shawn.pelerin@hotmail.fr>
2026-01-27 00:18:41 +0100

    Affichage

--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,51 +1,52 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CommonModule } from '@angular/common'; // Import requis
+import { CommonModule } from '@angular/common'; // Import indispensable
 import { FatigoonService } from '../../services/fatigoon.service';
 
 @Component({
-	selector: 'app-user-detail',
-	standalone: true,
-	imports: [CommonModule], // Ajoutez CommonModule ici
-	templateUrl: './user-detail.component.html'
+  selector: 'app-user-detail',
+  standalone: true,
+  imports: [CommonModule], // Ajoutez-le ici
+  templateUrl: './user-detail.component.html'
 })
 export class UserDetailComponent implements OnInit {
-	private route = inject(ActivatedRoute);
-	protected fatigoonService = inject(FatigoonService);
+  private route = inject(ActivatedRoute);
+  protected fatigoonService = inject(FatigoonService);
 
-	user: any;
-	ownerGuilds: any[] = [];
-	memberGuilds: any[] = [];
+  user: any;
+  ownerGuilds: any[] = [];
+  memberGuilds: any[] = [];
 
-	ngOnInit() {
-		const username = this.route.snapshot.paramMap.get('username');
-		if (username) {
-			this.fatigoonService.getUserByUsername(username).subscribe({
-				next: (userData) => {
-					this.user = userData;
-					this.loadGuildsData();
-				},
-				error: (err) => console.error("Erreur User:", err)
-			});
-		}
-	}
+  ngOnInit() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (username) {
+      this.fatigoonService.getUserByUsername(username).subscribe({
+        next: (userData) => {
+          this.user = userData;
+          this.loadGuildsData();
+        },
+        error: (err) => console.error("Erreur User:", err)
+      });
+    }
+  }
 
-	loadGuildsData() {
-		// Note: les IDs sont maintenant des strings grâce à votre modification backend
-		if (this.user.ownedGuildIds?.length > 0) {
-			this.fatigoonService.getGuildsByIds(this.user.ownedGuildIds).subscribe(data => {
-				this.ownerGuilds = data;
-			});
-		}
+  loadGuildsData() {
+    // Charger les guildes possédées
+    if (this.user.ownedGuildIds?.length > 0) {
+      this.fatigoonService.getGuildsByIds(this.user.ownedGuildIds).subscribe(data => {
+        this.ownerGuilds = data;
+      });
+    }
 
-		const otherGuildIds = this.user.guildIds?.filter(
-			(id: string) => !this.user.ownedGuildIds?.includes(id)
-		);
+    // Charger les autres guildes (exclure celles déjà dans owner)
+    const otherGuildIds = this.user.guildIds?.filter(
+      (id: string) => !this.user.ownedGuildIds?.includes(id)
+    );
 
-		if (otherGuildIds?.length > 0) {
-			this.fatigoonService.getGuildsByIds(otherGuildIds).subscribe(data => {
-				this.memberGuilds = data;
-			});
-		}
-	}
+    if (otherGuildIds?.length > 0) {
+      this.fatigoonService.getGuildsByIds(otherGuildIds).subscribe(data => {
+        this.memberGuilds = data;
+      });
+    }
+  }
 }
